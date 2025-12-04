f2db448950f20bc5be6dfe23b1f4eda4cdc9a1a9
Andrei Markouski <insawm@gmail.com>
2025-12-04 15:39:35 +0300

    feat: add album module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
