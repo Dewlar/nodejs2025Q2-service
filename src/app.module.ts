cc227c32022932bc4610f22074f28d12b19790b3
Andrei Markouski <insawm@gmail.com>
2025-12-04 17:50:02 +0300

    feat: add favs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AlbumModule, ArtistModule],
+  imports: [ConfigModule.forRoot(), AlbumModule, ArtistModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
