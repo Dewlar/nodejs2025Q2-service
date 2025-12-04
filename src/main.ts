3dbf8bde9f5d5ba78258b8beb3570897633de354
Andrei Markouski <insawm@gmail.com>
2025-12-04 16:50:38 +0300

    fix: swagger doc cannot find bundle and preset files with new version libs

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
@@ -7,9 +8,14 @@ import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT') || 4000;
+
+  app.useStaticAssets(
+    path.join(__dirname, '..', 'node_modules', 'swagger-ui-dist'),
+    { prefix: '/swagger-ui-assets' },
+  );
 
   const fileAPI = fs.readFileSync(
     path.join(__dirname, '../doc/api.yaml'),
@@ -17,7 +23,13 @@ async function bootstrap() {
   );
   const docFileAPI = yaml.load(fileAPI);
 
-  SwaggerModule.setup('doc', app, docFileAPI as OpenAPIObject);
+  SwaggerModule.setup('doc', app, docFileAPI as OpenAPIObject, {
+    customCssUrl: '/swagger-ui-assets/swagger-ui.css',
+    customJs: [
+      '/swagger-ui-assets/swagger-ui-bundle.js',
+      '/swagger-ui-assets/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   await app.listen(PORT, () => console.log('App is running on the port', PORT));
 }
