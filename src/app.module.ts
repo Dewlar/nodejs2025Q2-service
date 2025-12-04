ddde53d2f737f618d8f1e09bb7da4ed9297f73ab
Andrei Markouski <insawm@gmail.com>
2025-12-04 16:45:46 +0300

    feat: add artist module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AlbumModule],
+  imports: [ConfigModule.forRoot(), AlbumModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
