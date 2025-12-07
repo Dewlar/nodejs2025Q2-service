00f9db08ce656eb8d640dce635317e5bd92bda3c
Andrei Markouski <insawm@gmail.com>
2025-12-07 17:29:25 +0300

    feat: implement PrismaClient in the artist module

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../db/prisma.module';
 import { ArtistController } from './artist.controller';
 import { ArtistService } from './artist.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ArtistController],
   providers: [ArtistService],
   exports: [ArtistService],
