381fac3ac76071b20d878aee9e265c116e258685
Andrei Markouski <insawm@gmail.com>
2025-12-12 23:55:07 +0300

    fix: logging to file, add structured log

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,19 +1,40 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { LoggerService } from './logger.service';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  private logger = new Logger(LoggerMiddleware.name);
+  // standard logger - output to console
+  // private logger = new Logger(LoggerMiddleware.name);
+  // custom LoggerService - output in file and console
+  constructor(private readonly logger: LoggerService) {}
 
-  use(req: Request, res: Response, next: NextFunction) {
-    const { ip, method, url, statusCode } = req;
-    const userAgent = req.get('User-Agent') || '';
+  use(request: Request, response: Response, next: NextFunction) {
+    const { ip, method, originalUrl, query, body } = request;
+    const userAgent = request.get('User-Agent') || '';
+    const requestStart = Date.now();
 
-    req.on('finish', () => {
-      const contentLength = res.get('content-length');
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const processingTime = Date.now() - requestStart;
 
+      // simple log:
+      // const contentLength = response.get('content-length') ?? '-';
+      // this.logger.log(`${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} - ${processingTime}ms`);
+
+      // structured log:
       this.logger.log(
-        `${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+        JSON.stringify({
+          timestamp: new Date().toISOString(),
+          method,
+          originalUrl,
+          query,
+          body,
+          statusCode,
+          ip,
+          userAgent,
+          processingTime,
+        }),
       );
     });
 
--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,11 +1,11 @@
 import { ConsoleLogger, Injectable, LogLevel } from '@nestjs/common';
-import fs from 'fs/promises';
-import path from 'path';
-// import * as process from "node:process";
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { Stats } from 'fs';
 
 @Injectable()
 export class LoggerService extends ConsoleLogger {
-  private level: LogLevel[] = [];
+  private logLevels: LogLevel[] = [];
   private maxFileSize: number;
   private countersMap: Map<LogLevel, number> = new Map();
 
@@ -23,39 +23,38 @@ export class LoggerService extends ConsoleLogger {
   }
 
   log(message: any) {
-    if (!this.level.includes('log')) return;
+    if (!this.logLevels.includes('log')) return;
     this.writeLogs('log', this.logMessage(message)).then();
   }
 
   error(message: any, stack?: any) {
-    if (!this.level.includes('error')) return;
+    if (!this.logLevels.includes('error')) return;
     this.writeLogs('error', this.logMessage(message, stack)).then();
   }
 
   warn(message: any) {
-    if (!this.level.includes('warn')) return;
+    if (!this.logLevels.includes('warn')) return;
     this.writeLogs('warn', this.logMessage(message)).then();
   }
 
   debug(message: any) {
-    if (!this.level.includes('debug')) return;
+    if (!this.logLevels.includes('debug')) return;
     this.writeLogs('debug', this.logMessage(message)).then();
   }
 
   verbose(message: any) {
-    if (!this.level.includes('verbose')) return;
+    if (!this.logLevels.includes('verbose')) return;
     this.writeLogs('verbose', this.logMessage(message)).then();
   }
 
   private logLevelsInit() {
-    const logLevels = process.env.LOGGER_LEVELS.replace('[', '')
-      .replace('[', '')
-      .split(',');
+    const logLevelStr = process.env.LOGGER_LEVELS.replace(/^\[|\]$/g, '');
+    const logLevels: string[] = logLevelStr.split(',').map((l) => l.trim());
 
     if (Array.isArray(logLevels)) {
       logLevels.forEach((level) => {
         if (['log', 'error', 'warn', 'debug', 'verbose'].includes(level)) {
-          this.level.push(level as LogLevel);
+          this.logLevels.push(level as LogLevel);
           this.countersMap.set(level as LogLevel, 1);
         }
       });
@@ -63,6 +62,8 @@ export class LoggerService extends ConsoleLogger {
   }
 
   private logMessage(message: string, stack?: any) {
+    super.log(message);
+
     let msg = `${new Date().toISOString()}: ${message}`;
 
     if (stack) {
@@ -72,30 +73,34 @@ export class LoggerService extends ConsoleLogger {
   }
 
   private async writeLogs(logType: LogLevel, data: string) {
-    data += '\n';
-    const logFolderPath = path.resolve('/app/dist', 'logs');
-
+    const logFolderPath = path.resolve(process.cwd(), 'dist', 'logs');
     await fs.mkdir(logFolderPath, { recursive: true });
 
-    let counter = this.countersMap.get(logType) ?? 0;
-    let logFilePath = path.resolve(logFolderPath, `${logType}_${counter}.txt`);
+    try {
+      let counter = this.countersMap.get(logType) ?? 1;
+      let logFilePath = path.join(logFolderPath, `${logType}_${counter}.txt`);
 
-    fs.access(logFilePath)
-      .then(() => {
-        fs.stat(logFilePath).then((stats) => {
-          if (stats.size > this.maxFileSize) {
-            counter += 1;
-            this.countersMap.set(logType, counter);
+      // const existFileStats = await fs
+      //   .stat(logFolderPath)
+      //   .then((stat) => stat)
+      //   .catch((err) => (err.code === 'ENOENT' ? null : Promise.reject(err)));
 
-            logFilePath = path.resolve(
-              logFolderPath,
-              `${logType}_${counter}.txt`,
-            );
-          }
-        });
-      })
-      .catch((error) => console.log(error));
+      let existFileStats: Stats | null = null;
+      try {
+        existFileStats = await fs.stat(logFilePath);
+      } catch (err) {
+        if (err.code !== 'ENOENT') throw err;
+      }
 
-    await fs.writeFile(logFilePath, data, { flag: 'a' });
+      if (existFileStats && existFileStats.size > this.maxFileSize) {
+        counter += 1;
+        this.countersMap.set(logType, counter);
+        logFilePath = path.join(logFolderPath, `${logType}_${counter}.txt`);
+      }
+
+      await fs.writeFile(logFilePath, data + '\n', { flag: 'a' });
+    } catch (error) {
+      console.error('Error in writeLogs:', error);
+    }
   }
 }
